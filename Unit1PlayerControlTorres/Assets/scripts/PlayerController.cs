9b51e33407ad5666db7e0263b8e90f61cec46af9
Mariana <nano091306@gmail.com>
2024-03-06 13:50:32 -0800

    finished unit 1 started the extra
    complete unit one now i started with the extra work/ challenges. I already built a pyramid with crates and got vehicles to move towards me.

--- Unit1PlayerControlTorres/Assets/scripts/PlayerController.cs
+++ Unit1PlayerControlTorres/Assets/scripts/PlayerController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
+    // Private Variables
+    private float speed = 25.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -17,9 +19,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //this is where we get player input
         horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         // we'll move the vehicle foward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // we turn the vehicle
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
