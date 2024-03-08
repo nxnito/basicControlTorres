ccc978878b45ed70d7d02a9415ebf09057b0361f
Mariana <nano091306@gmail.com>
2024-03-08 13:48:47 -0800

    completed the project
    finished everrytthing and I already recorded the video and turned it in.

--- Unit1PlayerControlTorres/Assets/scripts/IncomingVehicles.cs
+++ Unit1PlayerControlTorres/Assets/scripts/IncomingVehicles.cs
@@ -7,7 +7,7 @@ public class IncomingVehicles : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
--- Unit1PlayerControlTorres/Assets/scripts/PlayerController.cs
+++ Unit1PlayerControlTorres/Assets/scripts/PlayerController.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Private Variables
-    private float speed = 25.0f;
-    private float turnSpeed = 25.0f;
+    private float speed = 30.0f;
+    private float turnSpeed = 50.0f;
     private float horizontalInput;
     private float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -27,5 +30,10 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // we turn the vehicle
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
